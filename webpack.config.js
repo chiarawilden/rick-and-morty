b755e37006018e2537e94439afc6200d9304aa8a
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-28 21:13:41 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "src/index.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.[contenthash].js",
+        assetModuleFilename: "[name][ext]",
+        clean: true
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: (__dirname, "public/index.html"),
+        })
+    ],
+    resolve: {
+        modules: [__dirname, "src", "node_modules"],
+        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: require.resolve("babel-loader"),
+            },
+            {
+                test: /\.css|scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.gif|ico|jpg|jpeg|png|svg$/,
+                type: "asset/resource",
+                use: ["file-loader"],
+            }
+        ]
+    }
+};
