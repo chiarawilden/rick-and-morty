35784cc793d9a0c142f747cd6a254b656387ba20
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-28 22:38:41 +0200

    Creates Home component with gradient background and styled Topbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Home from "./pages/Home";
 
 export default function App() {
     return (
-        <div>Hello React!</div>
+        <Home/>
     )
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+import Topbar from "../components/topbar/Topbar";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <Topbar/>
+        </div>
+    )
+}
