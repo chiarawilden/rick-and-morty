f89ca02ff425a7e48957b34afe2071ef52b74084
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-29 16:21:19 +0200

    Creates logic for filtering through Rick & Morty character api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./components/search/Search";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 
+
 export default function App() {
     return (
         <div className="app">
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./search.css";
-
+import axios from "axios";
+import "core-js";
+import regeneratorRuntime from "regenerator-runtime";
 
 export default function Search() {
+    const [characters, setCharacters] = useState([]);
+    const [filteredCharacters, setFilteredCharacters] = useState([]);
+ 
+    useEffect(() => {
+            const getCharacters = async () => {
+                const res = await axios.get("https://rickandmortyapi.com/api/character");
+                setCharacters(res.data.results);
+                console.log(res.data.results);
+            }
+            getCharacters();
+    }, []);
+
+    const filterData = event => {
+        const searchTerm = event.target.value;
+        const filteredData = characters.filter(character => {
+            return character.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        if (searchTerm === "") {
+            setFilteredCharacters([]);
+        } else {
+            setFilteredCharacters(filteredData);
+        }
+    };
+
     return (
         <div className="search">
             <form>
-                <input></input>
-                <i class="fas fa-search"></i>
+                <button type="submit">
+                    <i className="fas fa-search"></i>
+                </button>
+                <input type="text" placeholder="Search" onChange={filterData}></input>
+                <i className="fas fa-times"></i>
             </form>
+            {filteredCharacters.length !== 0 &&
+                <div className="search-result">
+                    {filteredCharacters.slice(0, 9).map(character => {
+                        return (
+                            <div className="search-item" key={character.id}>{character.name}</div>
+                        )
+                    })};
+                </div>
+            }
         </div>
     )
 }
